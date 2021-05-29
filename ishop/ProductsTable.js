50b1401320556222131cbe7a65c88fe89a9d2687
Olga <dicoun@gmail.com>
2021-05-30 00:08:37 +0300

    final_version

--- ishop/ProductsTable.js
+++ ishop/ProductsTable.js
@@ -4,22 +4,39 @@ var ProductsTable = React.createClass({
   
     render: function() {
   
-      var tableCode=[];
-      for ( var a=0; a<this.props.goods.length; a++ ) {
-        var product=this.props.goods[a];
-        var tableCode=        
-          React.DOM.div({key:product.code,className:'Row'},
-            React.DOM.span({className:'ProdName'},product.name),
-            React.DOM.span({className:'ProdPrice'},product.price),
-            React.DOM.span({className:'ProdUrl'},product.url),
-            React.DOM.span({className:'ProdRemainder'},product.remainder),
-          );
-          tableCode.push(tableCode);
-      }
+      var tableCode=[]; 
+        this.props.goods.forEach(function(product){
+            var tableCodeObj= 
+            React.DOM.tr({key:product.code,className:'Row'},
+                React.DOM.td({key:'ProdName '+ product.code},
+                    React.DOM.span({className:'ProdName'},product.name),
+                ),
+                React.DOM.td({key:'ProdPrice '+ product.code},
+                    React.DOM.span({className:'ProdPrice'},product.price),
+                ),
+                React.DOM.td({key:'ProdUrl '+ product.code},
+                    React.DOM.a({className:'ProdUrl', href:product.url}, 'Фото'),
+                ),
+                React.DOM.td({key:'ProdRemainder'+ product.code},
+                    React.DOM.span({className:'ProdRemainder'},product.remainder),
+                ),
+            );
+            tableCode.push(tableCodeObj);     
+        });
+      var tBody = React.DOM.tbody({key:'ProdTable'}, 
+        React.DOM.tr({key:'caption'},
+            React.DOM.th({}, 'Наименование товара'),
+            React.DOM.th({}, 'Цена товара'),
+            React.DOM.th({}, 'Фото товара'),
+            React.DOM.th({}, 'Товара в наличии'),
+        ),
+        tableCode);
+
       return React.DOM.div( {className:'ProductsTable'}, 
-        React.DOM.div( {className:'ShopName'}, this.props.name ),
-        React.DOM.div( {className:'Goods'}, tableCode ),
+        React.DOM.table( {className:'Goods'},
+            React.DOM.caption({className:'ShopName'}, this.props.name), 
+            tBody 
+        ),
       );
     },
-  
   });
